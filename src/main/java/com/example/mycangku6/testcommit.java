ab82ab6de9c7588a7a7a9b78c587d454f921cc88
chenjie <2089009549@qq.com>
2024-05-28 14:37:41 +0800

    第二次提交

--- src/main/java/com/example/mycangku6/testcommit.java
+++ src/main/java/com/example/mycangku6/testcommit.java
@@ -3,5 +3,6 @@ package com.example.mycangku6;
 public class testcommit {
     public static void main(String[] args) {
         System.out.println("我服了");
+        System.out.println("shabi");
     }
 }
