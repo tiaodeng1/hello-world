a5a9df7af2b8b6756721aef28b6a03a9ce62f99c
chenjie <2089009549@qq.com>
2024-05-28 14:32:10 +0800

    第一次提交

---
+++ src/main/java/com/example/mycangku6/Mycangku6Application.java
@@ -0,0 +1,13 @@
+package com.example.mycangku6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mycangku6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mycangku6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mycangku6/Mycangku6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycangku6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mycangku6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
